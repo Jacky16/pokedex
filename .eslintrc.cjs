c535c12eeb6459db56aa4388d52fe13e40e1f9c3
Adria Martinez <adriamartinez94@gmail.com>
2023-11-25 13:18:20 +0100

    Install pokemon library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,43 @@
 module.exports = {
-    extends: ["plugin:astro/recommended"],
-    overrides: [
-        {
-            files: ["*.astro"],
-            parser: "astro-eslint-parser",
-            parserOptions: {
-                parser: "@typescript-eslint/parser",
-                extraFileExtensions: [".astro"],
-            },
-        },
-    ],
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Enable this plugin
+      plugins: ["astro"],
+      env: {
+        // Enables global variables available in Astro components.
+        node: true,
+        "astro/astro": true,
+        es2020: true,
+      },
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+        // The script of Astro components uses ESM.
+        sourceType: "module",
+      },
+      rules: {
+        "astro/no-conflict-set-directives": "error",
+        "astro/no-unused-define-vars-in-style": "error",
+      },
+    },
+    {
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
+      env: {
+        browser: true,
+        es2020: true,
+      },
+      parserOptions: {
+        sourceType: "module",
+      },
+      rules: {
+        "prettier/prettier": "on",
+      },
+    },
+  ],
 };
